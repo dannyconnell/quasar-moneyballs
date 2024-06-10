64c8517bd03a9e67fbd6ea0ed592b75da34e4a5c
Danny Connell <sendstufftodanny@gmail.com>
2024-06-10 16:06:02 +0100

    4.1

--- src/pages/PageEntries.vue
+++ src/pages/PageEntries.vue
@@ -1,9 +1,61 @@
 <template>
-  <q-page class="flex flex-center">
-    Entries
+  <q-page>
+    <div class="q-pa-md">
+      <q-list
+        bordered
+        separator
+      >
+        <q-item
+          v-for="entry in entries"
+          :key="entry.id"
+        >
+          <q-item-section>
+            {{ entry.name }}
+          </q-item-section>
+
+          <q-item-section side>
+            {{ entry.amount }}
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </div>
   </q-page>
 </template>
 
 <script setup>
+
+  /*
+    imports
+  */
+  
+    import { ref } from 'vue'
+
+
+  /*
+    entries
+  */
+  
+    const entries = ref([
+      {
+        id: 'id1',
+        name: 'Salary',
+        amount: 4999.99
+      },
+      {
+        id: 'id2',
+        name: 'Rent',
+        amount: -999
+      },
+      {
+        id: 'id3',
+        name: 'Phone',
+        amount: -14.99
+      },
+      {
+        id: 'id4',
+        name: 'Unknown',
+        amount: 0
+      },
+    ])
 
 </script>
