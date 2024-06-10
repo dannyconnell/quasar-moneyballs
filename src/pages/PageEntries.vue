5df46bd1a24aafeec41c2f58781f3028c3a4faff
Danny Connell <sendstufftodanny@gmail.com>
2024-06-10 20:10:32 +0100

    4.7

--- src/pages/PageEntries.vue
+++ src/pages/PageEntries.vue
@@ -27,8 +27,18 @@
       </q-list>
     </div>
 
-    <q-footer>
-      <div class="row q-pa-sm q-col-gutter-sm">
+    <q-footer
+      class="bg-transparent"
+    >
+      <div class="row q-mb-sm q-px-md q-py-sm shadow-up-3">
+        <div class="col text-grey-7 text-h6">
+          Balance:
+        </div>
+        <div class="col text-grey-7 text-h6 text-right">
+          + $3,999.00
+        </div>
+      </div>
+      <div class="row q-px-sm q-pb-sm q-col-gutter-sm bg-primary">
         <div class="col">
           <q-input
             placeholder="Name"
