00520605391984b173f2cd3ee8f25ace9016fe02
Danny Connell <sendstufftodanny@gmail.com>
2024-06-10 16:33:51 +0100

    4.2

--- src/pages/PageEntries.vue
+++ src/pages/PageEntries.vue
@@ -14,7 +14,7 @@
           </q-item-section>
 
           <q-item-section side>
-            {{ entry.amount }}
+            {{ currencify(entry.amount) }}
           </q-item-section>
         </q-item>
       </q-list>
@@ -57,5 +57,27 @@
         amount: 0
       },
     ])
+
+  
+  /*
+    currencify
+  */
+  
+    function currencify(amount) {
+      // format: "+ $ 4,999.99" | "- $ 999.00"
+
+      let posNegSymbol = ''
+      if (amount > 0) posNegSymbol = '+'
+      else if (amount < 0) posNegSymbol = '-'
+
+      const currencySymbol = '$',
+            amountPositive = Math.abs(amount),
+            amountFormatted = amountPositive.toLocaleString('en-US', {
+              minimumFractionDigits: 2,
+              maximumFractionDigis: 2
+            })
+
+      return `${ posNegSymbol } ${ currencySymbol } ${ amountFormatted }`
+    }
 
 </script>
