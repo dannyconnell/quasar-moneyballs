f167f53fce3b44b5f6241a407a80beb370de239e
Danny Connell <sendstufftodanny@gmail.com>
2024-06-12 12:59:47 +0100

    8.3

---
+++ src/components/Entries/Entry.vue
@@ -0,0 +1,103 @@
+<template>
+  <q-slide-item
+    @right="onEntrySlideRight"
+    left-color="positive"
+    right-color="negative"
+  >
+    <!-- <template v-slot:left>
+      <q-icon name="done" />
+    </template> -->
+    <template v-slot:right>
+      <q-icon name="delete" />
+    </template>
+
+    <q-item>
+      <q-item-section
+        class="text-weight-bold"
+        :class="useAmountColorClass(entry.amount)"
+      >
+        {{ entry.name }}
+      </q-item-section>
+
+      <q-item-section
+        class="text-weight-bold"
+        :class="useAmountColorClass(entry.amount)"
+        side
+      >
+        {{ useCurrencify(entry.amount) }}
+      </q-item-section>
+    </q-item>
+
+  </q-slide-item>
+</template>
+
+<script setup>
+
+  /*
+    imports
+  */
+  
+    import { useQuasar } from 'quasar'
+    import { useStoreEntries } from 'src/stores/storeEntries'
+    import { useCurrencify } from 'src/use/useCurrencify'
+    import { useAmountColorClass } from 'src/use/useAmountColorClass'
+
+
+  /*
+    stores
+  */
+  
+    const storeEntries = useStoreEntries()
+
+
+  /*
+    props
+  */
+  
+    const props = defineProps({
+      entry: {
+        type: Object,
+        required: true
+      }
+    })  
+
+
+  /*
+    quasar
+  */
+  
+    const $q = useQuasar()  
+
+  /*
+    slide items
+  */
+  
+    const onEntrySlideRight = ({ reset }) => {
+      $q.dialog({
+        title: 'Delete Entry',
+        message: `
+          Delete this entry?
+          <div class="q-mt-md text-weight-bold ${ useAmountColorClass(props.entry.amount) }">
+            ${ props.entry.name } : ${ useCurrencify(props.entry.amount) }
+          </div>
+        `,
+        cancel: true,
+        persistent: true,
+        html: true,
+        ok: {
+          label: 'Delete',
+          color: 'negative',
+          noCaps: true
+        },
+        cancel: {
+          color: 'primary',
+          noCaps: true
+        }
+      }).onOk(() => {
+        storeEntries.deleteEntry(props.entry.id)
+      }).onCancel(() => {
+        reset()
+      })
+    }
+
+</script>
--- src/pages/PageEntries.vue
+++ src/pages/PageEntries.vue
@@ -6,38 +6,11 @@
         separator
       >
 
-        <q-slide-item
+        <Entry 
           v-for="entry in storeEntries.entries"
           :key="entry.id"
-          @right="onEntrySlideRight($event, entry)"
-          left-color="positive"
-          right-color="negative"
-        >
-          <!-- <template v-slot:left>
-            <q-icon name="done" />
-          </template> -->
-          <template v-slot:right>
-            <q-icon name="delete" />
-          </template>
-
-          <q-item>
-            <q-item-section
-              class="text-weight-bold"
-              :class="useAmountColorClass(entry.amount)"
-            >
-              {{ entry.name }}
-            </q-item-section>
-
-            <q-item-section
-              class="text-weight-bold"
-              :class="useAmountColorClass(entry.amount)"
-              side
-            >
-              {{ useCurrencify(entry.amount) }}
-            </q-item-section>
-          </q-item>
-
-        </q-slide-item>
+          :entry="entry"
+        />
 
       </q-list>
     </div>
@@ -57,12 +30,10 @@
     imports
   */
   
-    import { useQuasar } from 'quasar'
     import { useStoreEntries } from 'src/stores/storeEntries'
-    import { useCurrencify } from 'src/use/useCurrencify'
-    import { useAmountColorClass } from 'src/use/useAmountColorClass'
     import Balance from 'src/components/Entries/Balance.vue'
     import AddEntry from 'src/components/Entries/AddEntry.vue'
+    import Entry from 'src/components/Entries/Entry.vue'
 
 
   /*
@@ -70,45 +41,5 @@
   */
   
     const storeEntries = useStoreEntries()
-
-
-  /*
-    quasar
-  */
-  
-    const $q = useQuasar()  
-  
-
-  /*
-    slide items
-  */
-  
-    const onEntrySlideRight = ({ reset }, entry) => {
-      $q.dialog({
-        title: 'Delete Entry',
-        message: `
-          Delete this entry?
-          <div class="q-mt-md text-weight-bold ${ useAmountColorClass(entry.amount) }">
-            ${ entry.name } : ${ useCurrencify(entry.amount) }
-          </div>
-        `,
-        cancel: true,
-        persistent: true,
-        html: true,
-        ok: {
-          label: 'Delete',
-          color: 'negative',
-          noCaps: true
-        },
-        cancel: {
-          color: 'primary',
-          noCaps: true
-        }
-      }).onOk(() => {
-        storeEntries.deleteEntry(entry.id)
-      }).onCancel(() => {
-        reset()
-      })
-    }
 
 </script>
