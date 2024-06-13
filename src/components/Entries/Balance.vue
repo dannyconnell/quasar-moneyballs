f8f34fd688f5ba094cac0abb281d17187d066f67
Danny Connell <sendstufftodanny@gmail.com>
2024-06-13 10:28:51 +0100

    10.3

--- src/components/Entries/Balance.vue
+++ src/components/Entries/Balance.vue
@@ -7,7 +7,24 @@
       :class="useAmountColorClass(storeEntries.balance)"
       class="col text-h6 text-right"
     >
-      {{ useCurrencify(storeEntries.balance) }}
+      <div class="row">
+        <div class="col">
+          {{ useCurrencify(storeEntries.balance) }}
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col text-caption text-grey-6">
+          Paid: 
+          <span
+            class="text-weight-bold"
+            :class="useAmountColorClass(-1234.56)"
+          >
+            {{ useCurrencify(-1234.56) }}
+          </span>
+        </div>
+      </div>
+
     </div>
   </div>
 </template>
