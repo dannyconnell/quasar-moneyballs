7020477a9f81eab7799433cbf61bcc835151d979
Danny Connell <sendstufftodanny@gmail.com>
2024-06-19 10:29:03 +0100

    17.5

--- src-electron/electron-main.js
+++ src-electron/electron-main.js
@@ -5,6 +5,7 @@
   import { app, BrowserWindow, Menu } from 'electron'
   import path from 'path'
   import os from 'os'
+  import { menuTemplate } from './menu/menu'
 
 
 /*
@@ -12,116 +13,10 @@
 */
 
   // needed in case process is undefined under Linux
-  const platform = process.platform || os.platform()
+  const platform = process.platform || os.platform(),
+        menu = Menu.buildFromTemplate(menuTemplate)
   let mainWindow
 
-  const isMac = process.platform === 'darwin'
-
-  const menuTemplate = [
-    // { role: 'appMenu' }
-    ...(isMac
-      ? [{
-          label: app.name,
-          submenu: [
-            { role: 'about' },
-            { type: 'separator' },
-            { role: 'services' },
-            { type: 'separator' },
-            { role: 'hide' },
-            { role: 'hideOthers' },
-            { role: 'unhide' },
-            { type: 'separator' },
-            { role: 'quit' }
-          ]
-        }]
-      : []),
-    // { role: 'fileMenu' }
-    {
-      label: 'File',
-      submenu: [
-        isMac ? { role: 'close' } : { role: 'quit' }
-      ]
-    },
-    // { role: 'editMenu' }
-    {
-      label: 'Edit',
-      submenu: [
-        { role: 'undo' },
-        { role: 'redo' },
-        { type: 'separator' },
-        { role: 'cut' },
-        { role: 'copy' },
-        { role: 'paste' },
-        ...(isMac
-          ? [
-              { role: 'pasteAndMatchStyle' },
-              { role: 'delete' },
-              { role: 'selectAll' },
-              { type: 'separator' },
-              {
-                label: 'Speech',
-                submenu: [
-                  { role: 'startSpeaking' },
-                  { role: 'stopSpeaking' }
-                ]
-              }
-            ]
-          : [
-              { role: 'delete' },
-              { type: 'separator' },
-              { role: 'selectAll' }
-            ])
-      ]
-    },
-    // { role: 'viewMenu' }
-    {
-      label: 'View',
-      submenu: [
-        { role: 'reload' },
-        { role: 'forceReload' },
-        { role: 'toggleDevTools' },
-        { type: 'separator' },
-        { role: 'resetZoom' },
-        { role: 'zoomIn' },
-        { role: 'zoomOut' },
-        { type: 'separator' },
-        { role: 'togglefullscreen' }
-      ]
-    },
-    // { role: 'windowMenu' }
-    {
-      label: 'Window',
-      submenu: [
-        { role: 'minimize' },
-        { role: 'zoom' },
-        ...(isMac
-          ? [
-              { type: 'separator' },
-              { role: 'front' },
-              { type: 'separator' },
-              { role: 'window' }
-            ]
-          : [
-              { role: 'close' }
-            ])
-      ]
-    },
-    {
-      role: 'help',
-      submenu: [
-        {
-          label: 'Learn More',
-          click: async () => {
-            const { shell } = require('electron')
-            await shell.openExternal('https://electronjs.org')
-          }
-        }
-      ]
-    }
-  ]
-  
-  const menu = Menu.buildFromTemplate(menuTemplate)
-  
 
 /*
   app ready
