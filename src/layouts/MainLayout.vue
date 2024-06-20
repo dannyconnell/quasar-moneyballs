38e1c33c7bd000636136b0cd31bd768c7bc52782
Danny Connell <sendstufftodanny@gmail.com>
2024-06-20 10:08:46 +0100

    19.5

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -15,8 +15,10 @@
 
         <q-toolbar-title>
           <div class="absolute-center">
-            <q-icon name="savings" /> 
-            Moneyballs
+            <div class="toolbar-title-text">
+              <q-icon name="savings" /> 
+              Moneyballs
+            </div>
           </div>
         </q-toolbar-title>
 
